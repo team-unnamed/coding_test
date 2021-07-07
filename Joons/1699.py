65a232d754f6ea091b918a69935eb93bdd0eb91c
joons <joonamm@naver.com>
2021-07-08 00:11:00 +0900

    1699

--- Joons/1699.py
+++ Joons/1699.py
@@ -1,1 +1,21 @@
 # 제곱수의 합 푸는중입니당.
+# 최적의 마지막 항을 찾는 방식
+# 찾아진 값들은 저장. 
+import sys
+
+N = int(sys.stdin.readline().split()[0])+1
+
+dp = [0]*N
+
+for n in range(N):
+    dp[n] = n
+    # 갱신 부분
+    for i in range(n):
+        i_ = i*i
+        if i_ <= n:
+            if dp[n-i_]+1 < dp[n]:
+                dp[n] = dp[n-i_]+1
+        else:
+            break
+
+print(dp[-1])
