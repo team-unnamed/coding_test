4590cd1eece3f353259270df2344e9c025e107e8
Jiin Nam <jiinnam1428@gmail.com>
2021-08-02 17:11:10 +0900

    BOJ 12970

--- Jiin/12970.py
+++ Jiin/12970.py
@@ -15,16 +15,18 @@ A와 B의 합은 N, 곱은 K가 되도록 하면?
 import sys, math
 
 N, K = map(int, sys.stdin.readline().split())
-
 D = N**2 - 4*K  # x^2 + Nx + K 판별식
+
 if D < 0:
     print(-1)
-else:
-    x1 = abs(int((-N + math.sqrt(D)) / 2))  # 문자를 0.3개 넣을 수는 없으니까 소수점 자르기
-    x2 = abs(int((-N - math.sqrt(D)) / 2))
-    s = 'A'*x1 + 'B'*x2  # 일단 A를 x1개, B를 x2개씩 집어넣는다 (ABBBBBBBB)
 
-    lack = K-(x1*x2)  # AB가 K쌍 있어야 하는데 몇 쌍이 부족한지 체크
+else:
+    ROOT = math.sqrt(D)
+    x1 = -int((-N + ROOT) / 2)  # 문자를 0.3개 넣을 수는 없으니까 소수점 자르기
+    x2 = -int((-N - ROOT) / 2)  # N,K는 모두 양수니까 무조건 해가 음수겠지
+    s = 'A' * x1 + 'B' * x2  # 일단 A를 x1개, B를 x2개씩 집어넣는다 (ABBBBBBBB)
+
+    lack = K - (x1*x2)  # AB가 K쌍 있어야 하는데 몇 쌍이 부족한지 체크
     if lack > 0:
         s = s[:-lack] + 'A' + s[-lack:]  # lack쌍만큼 부족하다면 뒤에서부터 세어서 AB가 lack쌍이 되는 위치에 A 집어넣기 (ABBBBABBBB)
     print(s)
